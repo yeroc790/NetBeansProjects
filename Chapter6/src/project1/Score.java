61ed566c45229fbedd6fb53af85837c30cf10f60
yeroc790 <hansenc@ad.cs.southwesternadventist.edu>
2017-02-07 11:13:04 -0600

    Chapter 6 Project 1
    Finished Project 1, the diving score calculator

--- Chapter6/src/project1/Score.java
+++ Chapter6/src/project1/Score.java
@@ -1,5 +1,9 @@
 /*
- * What is this?
+ * Class for determining a total score
+ * The highest and lowest scores are thrown out
+ * The remaining scores are added together
+ * The sum is multiplied by the difficulty modifier
+ * The total is then multiplied by 0.6 to get the final score
  */
 package project1;
 
@@ -13,7 +17,9 @@ public class Score {
     private double finalScore; 
     
     public Score(double[] a, double diff){
-        scores = a;
+        double[] b;
+        b = orderArray(a);
+        scores = b;
         if((diff>1.2)&&(diff<3.8)){
             difficulty = diff;
         }else{
@@ -22,12 +28,41 @@ public class Score {
     }
     
     public Score(double[] a){
-        scores = a;
+        double[] b;
+        b = orderArray(a);
+        scores = b;
         difficulty = 1.2;
     }
     
     public void setScores(double[] a){
-        scores = a;
+        double[] b;
+        b = orderArray(a);
+        scores = b;
+    }
+    
+    private double[] orderArray(double[] a){
+        double[] b = new double[a.length];
+        boolean swap = false;
+        double temp;
+        
+        for (int i = 0; i < a.length; i++) {
+            b[i] = a[i];
+        }
+        
+        do{
+            swap = false;
+            for (int i = 0; i < b.length; i++) {
+                if((i+1)!=b.length){
+                    if(b[i]<b[i+1]){
+                        temp = b[i];
+                        b[i] = b[i+1];
+                        b[i+1] = temp;
+                        swap = true;
+                    }
+                }    
+            }
+        }while(swap==true);
+        return b;
     }
     
     public void setDifficulty(double diff){
@@ -57,5 +92,20 @@ public class Score {
         }
         System.out.println("Difficulty: " + difficulty);
         System.out.println("Final Score: " + finalScore);
+    }
+    
+    
+    
+    public void calculateFinalScore(){
+        double num = 0;
+        for (int i = 0; i < scores.length; i++) {
+            if((i!=0)&&(i!=(scores.length-1))) //tossing out the highest and lowest scores
+            {
+                num += scores[i];
+            }
+        }
+        num *= difficulty;
+        num *= 0.6;
+        finalScore = num;
     }
 }
--- Chapter6/src/project1/project1.java
+++ Chapter6/src/project1/project1.java
@@ -9,11 +9,10 @@ package project1;
  */
 public class project1 {
     public static void main(String[] args) {
-        double[] a = {10,2.5,3.9};
+        double[] a = {10,2.5,3.9,5.8,8.2,3.6};
         Score myScore = new Score(a, 1.2);
         
-        myScore.setFinalScore(6.8);
-        myScore.displayScore(); //things are working so far
-        //need to build the score calculation method
+        myScore.calculateFinalScore();
+        myScore.displayScore();
     }
 }
